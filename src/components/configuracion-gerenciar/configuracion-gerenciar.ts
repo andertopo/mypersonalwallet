ee26e3398897d85a9c3bd88baba07120e76afeff
andertopo <andersontorres9012@gmail.com>
2018-08-22 16:08:03 -0500

    Se agregan acciones para el item categorias, cuenta y etiquetas

--- src/components/configuracion-gerenciar/configuracion-gerenciar.ts
+++ src/components/configuracion-gerenciar/configuracion-gerenciar.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { CuentaPage } from '../../pages/cofiguracion/cuenta/cuenta';
+import { EtiquetaPage } from '../../pages/cofiguracion/etiqueta/etiqueta';
+import { CategoriaPage } from '../../pages/cofiguracion/categoria/categoria';
 
 @Component({
   selector: 'configuracion-gerenciar',
@@ -15,6 +17,16 @@ export class ConfiguracionGerenciarComponent {
   goCuenta() {
     console.log("nos vamos a cuenta");
     this.navCtrl.push(CuentaPage);
+  }
+
+  goEtiqueta() {
+    console.log("nos vamos a etiquetas");
+    this.navCtrl.push(EtiquetaPage);
+  }
+
+  goCategoria() {
+    console.log("nos vamos a categorias");
+    this.navCtrl.push(CategoriaPage);
   }
 
 }
