ce2b2773ce94e8c2e45474d36272e9006b620646
andertopo <andersontorres9012@gmail.com>
2018-08-22 16:13:12 -0500

    Se agrega popover para mostrar menu de acciones sobre categoria

---
+++ src/pages/cofiguracion/categoria/opciones-categoria-popover/opciones-categoria-popover.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams, PopoverController, ViewController } from 'ionic-angular';
+import { MoverTransaccionesPopoverPage } from '../mover-transacciones-popover/mover-transacciones-popover';
+import { CategoriaTransaccion } from '../../../../objects/CategoriaTransaccion';
+
+/**
+ * Generated class for the OpcionesCategoriaPopoverPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-opciones-categoria-popover',
+  templateUrl: 'opciones-categoria-popover.html',
+})
+export class OpcionesCategoriaPopoverPage {
+  public categoria: CategoriaTransaccion;
+
+  constructor(public viewCtrl: ViewController, public navCtrl: NavController, public navParams: NavParams, public popoverCtrl: PopoverController) {
+    this.categoria = new CategoriaTransaccion();
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad OpcionesCategoriaPopoverPage');
+    this.categoria = this.navParams.get('categoria');
+  }
+
+  openMover() {
+    this.viewCtrl.dismiss();
+    let popover = this.popoverCtrl.create(MoverTransaccionesPopoverPage, {categoria: this.categoria}, {cssClass: 'popover-center popover-busqueda'});
+    popover.present();
+  }
+
+}
