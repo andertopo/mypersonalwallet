bfbf2677c672f6a99a85b4cfe23d0f4fbb3fbba2
andertopo <andersontorres9012@gmail.com>
2018-08-22 16:14:09 -0500

    se modifica pantalla de nueva cuenta, se agregan validaciones

--- src/pages/cofiguracion/cuenta/cuenta-nueva/cuenta-nueva.ts
+++ src/pages/cofiguracion/cuenta/cuenta-nueva/cuenta-nueva.ts
@@ -1,23 +1,29 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, PopoverController } from 'ionic-angular';
+import { NavController, NavParams, PopoverController, ToastController } from 'ionic-angular';
 import { TipoCuentaOpcionesPage } from './tipo-cuenta-opciones/tipo-cuenta-opciones';
 import { CuentaTransaccion } from '../../../../objects/CuentaTransaccion';
 import { ColoresCuentaPopoverPage } from './colores-cuenta-popover/colores-cuenta-popover';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { CuentaProvider } from '../../../../providers/cuenta/cuenta-provider';
 
 @Component({
   selector: 'page-cuenta-nueva',
   templateUrl: 'cuenta-nueva.html',
 })
 export class CuentaNuevaPage {
-  private tipoCuenta: string;
-  public cuenta:CuentaTransaccion;
+  public formCuenta: FormGroup;
+  public cuenta: CuentaTransaccion;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public popoverCtrl: PopoverController) {
-    this.tipoCuenta = "dinero";
+  constructor(public navCtrl: NavController, public navParams: NavParams, public popoverCtrl: PopoverController, fb: FormBuilder, public cuentaProvider: CuentaProvider, public toastCtrl: ToastController) {
     this.cuenta = new CuentaTransaccion();
     this.cuenta.itemGui.setIcono('home');
     this.cuenta.itemGui.setColor('warning');
     this.cuenta.itemGui.setNombre('dinero');
+
+    this.formCuenta = fb.group({
+      'nombre': [null, Validators.required],
+      'valorInicial': [null, Validators.compose([Validators.required, Validators.pattern('[0-9]*')])]
+    });
   }
 
   ionViewDidLoad() {
@@ -26,10 +32,9 @@ export class CuentaNuevaPage {
 
   public openOpciones() {
     let popover = this.popoverCtrl.create(TipoCuentaOpcionesPage, {}, {});
-    popover.present({ev: event});
+    popover.present({ ev: event });
     popover.onDidDismiss(data => {
-      if(data) {
-        this.tipoCuenta = data.tipoCuenta;
+      if (data) {
         this.cuenta.itemGui.setNombre(data.tipoCuenta);
       }
     });
@@ -37,17 +42,38 @@ export class CuentaNuevaPage {
 
   public openColor() {
     let popover = this.popoverCtrl.create(ColoresCuentaPopoverPage, {}, {});
-    popover.present({ev: event});
+    popover.present({ ev: event });
     popover.onDidDismiss(data => {
-      if(data) {
-        this.tipoCuenta = data.color;
+      if (data) {
         this.cuenta.itemGui.setColor(data.color);
       }
     });
   }
 
   public crear() {
-    console.log("devolviendonos");
-    this.navCtrl.pop();
+    if (this.formCuenta.valid) {
+      this.cuenta.saldoTotal = this.formCuenta.value.valorInicial;
+      this.cuenta.itemGui.getNombre = this.formCuenta.value.nombre;
+      let cuentaCreada: boolean = this.cuentaProvider.crearCuenta(this.cuenta);
+      let mensaje: string = 'La cuenta ha sido creada';
+      if (!cuentaCreada) {
+        mensaje = 'Ha ocurrido un error al crear la cuenta';
+      }
+      let toast = this.toastCtrl.create({
+        message: mensaje,
+        duration: 3000,
+        position: 'middle'
+      });
+      toast.present();
+
+      toast.onDidDismiss(() => {
+        if(cuentaCreada) {
+          this.navCtrl.pop();
+        }
+      });
+    } else {
+      console.log(this.formCuenta.hasError);
+    }
   }
+
 }
