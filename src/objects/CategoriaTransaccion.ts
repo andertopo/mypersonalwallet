173313acc7574f4207cfd9b15db1fc2c5fd0a310
andertopo <andersontorres9012@gmail.com>
2018-08-22 16:08:47 -0500

    Se modifica objeto de transacciones

--- src/objects/CategoriaTransaccion.ts
+++ src/objects/CategoriaTransaccion.ts
@@ -1,50 +1,22 @@
+import { SelectFilterItem } from "./SelectFilterItem";
+
 export class CategoriaTransaccion {
-  private nombre:string;
-  private icono:string;
-  private color:string;
-  private selected:boolean;
+  public id:number;
+  public itemGui:SelectFilterItem;
+  public tipo:string; //si es ingreso o gasto
 
   static crearCategoria(nombre:string, icono:string, color:string, selected:boolean) {
     let categoria = new CategoriaTransaccion();
-    categoria.setNombre(nombre);
-    categoria.setIcono(icono);
-    categoria.setColor(color);
-    categoria.setSelected(selected);
+    categoria.itemGui.setNombre(nombre);
+    categoria.itemGui.setIcono(icono);
+    categoria.itemGui.setColor(color);
+    categoria.itemGui.setSelected(selected);
     console.log(categoria);
+    categoria.tipo = 'gasto';
     return categoria;
   }
 
-  public constructor() {}
-
-  public setNombre(nom:string) {
-    this.nombre = nom;
-  }
-
-  public getNombre() {
-    return this.nombre;
-  }
-
-  public setIcono(icon:string) {
-    this.icono = icon;
-  }
-
-  public getIcono() {
-    return this.icono;
-  }
-
-  public setColor(col:string) {
-    this.color = col;
-  }
-
-  public getColor() {
-    return this.color;
-  }
-
-  public setSelected(selected:boolean) {
-    this.selected = selected;
-  }
-
-  public isSelected() {
-    return this.selected;
+  public constructor() {
+    this.itemGui = new SelectFilterItem();
   }
 }
