ae0050867be7061af59b910a02b2c778ad58f58f
andertopo <andersontorres9012@gmail.com>
2018-08-22 16:07:06 -0500

    Se agregan componentes para los gastos

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -4,22 +4,29 @@ import { ProgressBarComponent } from './progress-bar/progress-bar';
 import { ConfiguracionPerfilComponent } from './configuracion-perfil/configuracion-perfil';
 import { ConfiguracionGerenciarComponent } from './configuracion-gerenciar/configuracion-gerenciar';
 import { ConfiguracionGeneralComponent } from './configuracion-general/configuracion-general';
+import { CategoriaGastosComponent } from './categoria-gastos/categoria-gastos';
+import { WalletSelectComponent } from './wallet-select/wallet-select';
 @NgModule({
 	declarations: [ProgressBarComponent,
     ConfiguracionPerfilComponent,
     ConfiguracionGerenciarComponent,
     ConfiguracionGeneralComponent,
+    CategoriaGastosComponent,
+    WalletSelectComponent,
   ],
 	imports: [
     IonicPageModule.forChild(ProgressBarComponent),
     IonicPageModule.forChild(ConfiguracionPerfilComponent),
     IonicPageModule.forChild(ConfiguracionGerenciarComponent),
-    IonicPageModule.forChild(ConfiguracionGeneralComponent)
+    IonicPageModule.forChild(ConfiguracionGeneralComponent),
+    IonicPageModule.forChild(CategoriaGastosComponent)
   ],
 	exports: [ProgressBarComponent,
     ConfiguracionPerfilComponent,
     ConfiguracionGerenciarComponent,
     ConfiguracionGeneralComponent,
+    CategoriaGastosComponent,
+    WalletSelectComponent,
   ]
 })
 export class ComponentsModule {}
