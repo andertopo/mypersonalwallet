058e22200a5536bcf933e32087372e822ef77a43
andertopo <andersontorres9012@gmail.com>
2018-08-22 16:14:39 -0500

    se modifica lógica de colores de cuenta, se generan desde el controlador

--- src/pages/cofiguracion/cuenta/cuenta-nueva/colores-cuenta-popover/colores-cuenta-popover.ts
+++ src/pages/cofiguracion/cuenta/cuenta-nueva/colores-cuenta-popover/colores-cuenta-popover.ts
@@ -6,15 +6,32 @@ import { NavController, NavParams, ViewController } from 'ionic-angular';
   templateUrl: 'colores-cuenta-popover.html',
 })
 export class ColoresCuentaPopoverPage {
+  public colors:Array<any>;
 
   constructor(public viewCtrl: ViewController, public navCtrl: NavController, public navParams: NavParams) {
+    this.colors = new Array();
+    let arrayColors = ['primary', 'secondary', 'warning', 'danger', 'azulPrimario', 'azul', 'green', 'beige', 'violeta', 'orange'];
+    let contColor = 0;
+    for(let i=0; i<6; i++) {
+      this.colors.push(new Array());
+      for(let j=0; j<5; j++) {
+        this.colors[i].push({
+          color: arrayColors[contColor],
+          selected: (this.navParams.get('selectedColor') && arrayColors[contColor] == this.navParams.get('selectedColor')) ? true : false
+        });
+        contColor++;
+      }
+      contColor++;
+    }
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ColoresCuentaPopoverPage');
+
   }
 
   public select(color:string) {
+    console.log('color', color);
     this.viewCtrl.dismiss({color: color});
   }
 
