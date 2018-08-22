f00c4f0c0a6480680c983d1df366880712731391
andertopo <andersontorres9012@gmail.com>
2018-08-22 16:12:22 -0500

    Se agrega popover para ver el listado de categorias

---
+++ src/pages/cofiguracion/categoria/nueva-subcategoria-popover/listado-categorias-popover/listado-categorias-popover.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams, ViewController } from 'ionic-angular';
+import { CategoriasProvider } from '../../../../../providers/categorias/categorias-provider';
+import { CategoriaTransaccion } from '../../../../../objects/CategoriaTransaccion';
+
+@Component({
+  selector: 'page-listado-categorias-popover',
+  templateUrl: 'listado-categorias-popover.html',
+})
+export class ListadoCategoriasPopoverPage {
+  public categorias: Array<CategoriaTransaccion>;
+  public categoriasFiltradas: Array<CategoriaTransaccion>;
+
+  constructor(public viewCtrl: ViewController, public navCtrl: NavController, public navParams: NavParams, public categoriaProvider: CategoriasProvider) {
+    console.log("cargando categorias", this.categoriaProvider.obtenerCategorias());
+    this.categorias = this.categoriaProvider.obtenerCategorias();
+    this.categoriasFiltradas = this.categorias;
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ListadoCategoriasPopoverPage');
+    console.log(this.categorias);
+  }
+
+  filtrar(event: any) {
+    let categoriaNombre = event.target.value;
+    this.categoriasFiltradas = this.categorias.filter(categoria => { return categoria["nombre"].startsWith(categoriaNombre)});
+  }
+
+  seleccionar(categoria:CategoriaTransaccion) {
+    this.viewCtrl.dismiss({categoria: categoria});
+  }
+
+}
