45588a715647ed1d48c34dbec4903b2611b06271
andertopo <andersontorres9012@gmail.com>
2018-12-14 14:07:17 -0500

    Se agrga funcionalidades para crear, editar eliminar categoria y subcategoria

--- src/pages/cofiguracion/categoria/opciones-categoria-popover/opciones-categoria-popover.ts
+++ src/pages/cofiguracion/categoria/opciones-categoria-popover/opciones-categoria-popover.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, PopoverController, ViewController } from 'ionic-angular';
+import { NavController, NavParams, PopoverController, ViewController, ToastController } from 'ionic-angular';
 import { MoverTransaccionesPopoverPage } from '../mover-transacciones-popover/mover-transacciones-popover';
 import { CategoriaTransaccion } from '../../../../objects/CategoriaTransaccion';
+import { IconosCategoriaPopoverPage } from '../iconos-categoria-popover/iconos-categoria-popover';
+import { CategoriasProvider } from '../../../../providers/categorias/categorias-provider';
+import { NuevaCategoriaPopoverPage } from '../nueva-categoria-popover/nueva-categoria-popover';
 
 /**
  * Generated class for the OpcionesCategoriaPopoverPage page.
@@ -17,7 +20,7 @@ import { CategoriaTransaccion } from '../../../../objects/CategoriaTransaccion';
 export class OpcionesCategoriaPopoverPage {
   public categoria: CategoriaTransaccion;
 
-  constructor(public viewCtrl: ViewController, public navCtrl: NavController, public navParams: NavParams, public popoverCtrl: PopoverController) {
+  constructor(public viewCtrl: ViewController, public navCtrl: NavController, public navParams: NavParams, public popoverCtrl: PopoverController, public toastCtrl: ToastController, public categoriaProvider: CategoriasProvider) {
     this.categoria = new CategoriaTransaccion();
   }
 
@@ -30,6 +33,47 @@ export class OpcionesCategoriaPopoverPage {
     this.viewCtrl.dismiss();
     let popover = this.popoverCtrl.create(MoverTransaccionesPopoverPage, {categoria: this.categoria}, {cssClass: 'popover-center'});
     popover.present();
+  }
+
+  editar() {
+    let popover = this.popoverCtrl.create(NuevaCategoriaPopoverPage, {tipo: this.categoria.tipo, categoria: this.categoria}, {});
+    popover.present();
+    popover.onDidDismiss(data => {
+      if (data) {
+        this.categoria = data.categoria;
+        this.viewCtrl.dismiss({categoria: this.categoria});
+      }
+    });
+  }
+
+  openIconos() {
+    let popover = this.popoverCtrl.create(IconosCategoriaPopoverPage, {color: 'opaque'}, {});
+    popover.present();
+    popover.onDidDismiss(data => {
+      if (data) {
+        console.log("seleccionando ", data);
+        this.categoria.itemGui.setIcono(data.icono);
+        this.categoriaProvider.guardarCategoria(this.categoria, true).then(resp => {
+          console.log("se almacenó bien el cambio de ícono");
+          this.viewCtrl.dismiss({categoria: this.categoria});
+        }).catch(err => {
+          this.showMessage('Ha ocurrido un error al crear la categoria', false);
+        });
+      }
+    });
+  }
+
+  showMessage(mensaje: string, created:boolean) {
+    let toast = this.toastCtrl.create({
+      message: mensaje,
+      duration: 2000,
+      position: 'middle'
+    });
+    toast.present();
+
+    toast.onDidDismiss(() => {
+      
+    });
   }
 
 }
--- src/providers/categorias/categorias-provider.ts
+++ src/providers/categorias/categorias-provider.ts
@@ -1,26 +1,78 @@
 import { Injectable } from '@angular/core';
 import { CategoriaTransaccion } from '../../objects/CategoriaTransaccion';
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
 
 @Injectable()
 export class CategoriasProvider {
   public categorias: Array<CategoriaTransaccion>;
 
-  constructor() {
+  constructor(public sqlite: SQLite) {
     this.categorias = new Array();
-  }
-
-  public generarCategorias() {
-    this.categorias.push(CategoriaTransaccion.crearCategoria('moto', 'car', 'warning', false, 'gasto'));
-    this.categorias.push(CategoriaTransaccion.crearCategoria('alimentacion', 'restaurant', 'primary', false, 'gasto'));
-    this.categorias.push(CategoriaTransaccion.crearCategoria('arriendo', 'home', 'secondary', false, 'gasto'));
   }
 
   public obtenerCategorias() {
     return this.categorias;
   }
 
+  public guardarCategoria(categoria:CategoriaTransaccion, isEdit:boolean) {
+    if(isEdit) {
+      console.log("editando", categoria); 
+      return this.editarCategoria(categoria);
+    } else {
+      console.log("creando", categoria);
+      return this.crearCategoria(categoria)
+    }
+  }
+
   public crearCategoria(categoria:CategoriaTransaccion) {
-    return true;
+    return new Promise((resolve, reject) => {
+      this.sqlite.create({
+        name: 'pesonalWallet.db',
+        location: 'default'
+      }).then((db: SQLiteObject) => {
+        console.log("se creo la base de datos");
+        db.executeSql('INSERT INTO tbl_categorias (nombre, icono, color, tipo, padre_categoria_id) VALUES(?,?,?,?,?)', [categoria.itemGui.nombre, categoria.itemGui.icono, categoria.itemGui.color, categoria.tipo, categoria.padreId]).then(resp => {
+          categoria.id = resp.insertId;
+          if(!categoria.padreId) {
+            this.categorias.push(categoria);
+          }
+          resolve(resp);
+          console.log("se insertó el registro", resp);
+        }).catch(err => {
+          console.log("no se insertó el registro en BD", err)
+          reject("no se insertó el registro en BD")
+        });
+      }).catch(e => {
+        console.log(e);
+        reject('no abre la base de datos');
+      });
+    });
+  }
+
+  public editarCategoria(categoria:CategoriaTransaccion) {
+    return new Promise((resolve, reject) => {
+      this.sqlite.create({
+        name: 'pesonalWallet.db',
+        location: 'default'
+      }).then((db: SQLiteObject) => {
+        console.log("se creo la base de datos");
+        db.executeSql('UPDATE tbl_categorias SET nombre = ?, icono = ?, color = ?, tipo = ?, padre_categoria_id = ? WHERE id_categoria = ?', [categoria.itemGui.nombre, categoria.itemGui.icono, categoria.itemGui.color, categoria.tipo, categoria.padreId, categoria.id]).then(resp => {
+          for(let index=0; index<this.categorias.length; index++) {
+            if(this.categorias[index].id == categoria.id) {
+              this.categorias[index] = categoria;
+            }
+          }
+          resolve(resp);
+          console.log("se actualizó el registro", resp);
+        }).catch(err => {
+          console.log("no se actualizó el registro en BD", err)
+          reject("no se acualizó el registro en BD")
+        });
+      }).catch(e => {
+        console.log(e);
+        reject('no abre la base de datos');
+      });
+    });
   }
 
   public moverTransacciones(categoriaOrigen:CategoriaTransaccion, categoriaDestino:CategoriaTransaccion) {
