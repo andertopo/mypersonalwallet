fc4df3c8041fac6e71a4c42b5bb74df078323839
andertopo <andersontorres9012@gmail.com>
2019-01-14 17:59:00 -0500

    se agrega componente para registro de transacciones

--- src/components/registro-ingreso-gasto/registro-ingreso-gasto.ts
+++ src/components/registro-ingreso-gasto/registro-ingreso-gasto.ts
@@ -1,4 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { CategoriasProvider } from '../../providers/categorias/categorias-provider';
+import { CategoriaTransaccion } from '../../objects/CategoriaTransaccion';
+import { CuentaProvider } from '../../providers/cuenta/cuenta-provider';
+import { CuentaTransaccion } from '../../objects/CuentaTransaccion';
 
 /**
  * Generated class for the RegistroIngresoGastoComponent component.
@@ -12,9 +16,13 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class RegistroIngresoGastoComponent implements OnInit {
   @Input() tipoTransaccion: string;
+  public categoriaSeleccionada: CategoriaTransaccion;
+  public cuentaSeleccionada: CuentaTransaccion;
 
-  constructor() {
+  constructor(public categoriasProvider: CategoriasProvider, public cuentasProvider:CuentaProvider) {
     console.log('Hello RegistroIngresoGastoComponent Component');
+    this.categoriaSeleccionada = this.categoriasProvider.categorias[0];
+    this.cuentaSeleccionada = this.cuentasProvider.cuentas[0];
   }
 
   ngOnInit() {
