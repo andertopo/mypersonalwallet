b907664cfe084a7e96f87730a16b25653877397c
andertopo <andersontorres9012@gmail.com>
2019-01-14 09:46:42 -0500

    Se eliminan métodos innecesarios

--- src/providers/etiqueta/etiqueta-provider.ts
+++ src/providers/etiqueta/etiqueta-provider.ts
@@ -11,12 +11,6 @@ export class EtiquetaProvider {
     this.etiquetas = new Array();
   }
 
-  private generarEtiquetas() {
-    this.etiquetas.push(SelectFilterItem.crearSelectFilterItem('desayuno', 'tag', '', false));
-    this.etiquetas.push(SelectFilterItem.crearSelectFilterItem('viaje', 'tag', '', false));
-    this.etiquetas.push(SelectFilterItem.crearSelectFilterItem('otros', 'tag', '', false));
-  }
-
   public obtenerEtiquetas() {
     //this.generarEtiquetas();
     return this.etiquetas;
