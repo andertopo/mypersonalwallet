56b65d4562eafc071adc40ad83a5d059553d1563
andertopo <andersontorres9012@gmail.com>
2019-01-15 17:47:20 -0500

    Se agrega ;

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 import { InicioPage } from '../inicio/inicio';
 import { ConfiguracionPage } from '../cofiguracion/configuracion';
-import { TransaccionesPage } from '../transacciones/transacciones'
+import { TransaccionesPage } from '../transacciones/transacciones';
 @Component({
   templateUrl: 'tabs.html'
 })
