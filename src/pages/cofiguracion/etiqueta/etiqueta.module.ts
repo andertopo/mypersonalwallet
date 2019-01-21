87469bc025521ab6973179b6bd4ffc18bbd256ef
andertopo <andersontorres9012@gmail.com>
2019-01-21 18:02:24 -0500

    Se modifica las etiquetas para mostrar mensaje cuando no hay

--- src/pages/cofiguracion/etiqueta/etiqueta.module.ts
+++ src/pages/cofiguracion/etiqueta/etiqueta.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { EtiquetaPage } from './etiqueta';
+import { SimpleSelectEtiquetaPopoverPage } from './simple-select-etiqueta-popover/simple-select-etiqueta-popover';
 
 @NgModule({
   declarations: [
     EtiquetaPage,
+    SimpleSelectEtiquetaPopoverPage
   ],
   imports: [
     IonicPageModule.forChild(EtiquetaPage),
+    IonicPageModule.forChild(SimpleSelectEtiquetaPopoverPage),
   ],
 })
 export class EtiquetaPageModule {}
