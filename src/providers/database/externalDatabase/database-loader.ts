40530c37449554442011091c38ce1fa358a28de4
andertopo <andersontorres9012@gmail.com>
2019-01-24 00:15:46 -0500

    se implementa capa de acceso a datos

---
+++ src/providers/database/externalDatabase/database-loader.ts
@@ -0,0 +1,203 @@
+import { Transaccion } from './../../../objects/Transaccion';
+import { Injectable } from '@angular/core';
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
+import { HttpClient } from '@angular/common/http';
+import { SQLitePorter } from '@ionic-native/sqlite-porter';
+import 'rxjs/add/operator/map';
+import { BehaviorSubject } from 'rxjs/Rx';
+import { CategoriasProvider } from '../../categorias/categorias-provider';
+import { CategoriaTransaccion } from '../../../objects/CategoriaTransaccion';
+import { CuentaProvider } from '../../cuenta/cuenta-provider';
+import { CuentaTransaccion } from '../../../objects/CuentaTransaccion';
+import { EtiquetaProvider } from '../../etiqueta/etiqueta-provider';
+import { SelectFilterItem } from '../../../objects/SelectFilterItem';
+import { Platform } from 'ionic-angular';
+import { ExternalTransacccionDAOProvider } from '../externalDatabase/external-transacciones-dao-provider';
+import { TabProvider } from '../../tab/tab-provider';
+
+@Injectable()
+export class DatabaseLoaderProvider {
+  private databaseReady: BehaviorSubject<boolean>;
+  public db: SQLiteObject;
+
+  constructor(public platform: Platform, 
+    private sqlite: SQLite, 
+    private sqliteporter: SQLitePorter, 
+    private http: HttpClient, 
+    public categoriaProvider: CategoriasProvider, 
+    public cuentaProvider:CuentaProvider, 
+    public etiquetaProvider:EtiquetaProvider, 
+    public tabProvider: TabProvider, 
+    public externalTransacccionDAOProvider: ExternalTransacccionDAOProvider)
+    {
+    console.log('Hello DatabaseProvider Provider');
+    this.databaseReady = new BehaviorSubject(false);
+    this.checkDatabase();
+  }
+
+  public isBdReady() {
+    return this.databaseReady.asObservable();
+  }
+
+  private checkDatabase() {
+    if(this.platform.is('core') || this.platform.is('mobileweb')) {
+      console.log("es por web, toca llamar al servicio remoto");
+      this.checkRemoteDatabase();
+    } else {
+      this.checkLocalDatabase();
+    }
+  }
+
+  private checkRemoteDatabase() {
+    this.http.get('/remote/categorias').subscribe(categorias => {
+      console.log("categorias: ", categorias);
+      for (let categoryKey in categorias) {
+        let categoria = categorias[categoryKey];
+        let categoriaTransaccion = CategoriaTransaccion.crearCategoria(categoria.id_categoria, categoria.nombre, categoria.icono, categoria.color, false, categoria.tipo, categoria.padre_categoria_id);
+        this.organizarSubcategorias(categoriaTransaccion);
+      }
+      this.cargarCuentas();
+    });
+  }
+
+  private cargarCuentas() {
+    console.log("cargando cuentas...");
+    this.http.get('/remote/cuentas').subscribe(cuentas => {
+      console.log("cuentas: ", cuentas);
+      for (let cuentaKey in cuentas) {
+        let cuenta = cuentas[cuentaKey];
+        let cuentaTransaccion = CuentaTransaccion.crearCuenta(cuenta.id_cuenta, cuenta.nombre, cuenta.icono, cuenta.color, false, cuenta.tipo, cuenta.ingresos, cuenta.gastos, cuenta.transacciones, null, cuenta.saldo_total);
+        this.cuentaProvider.cuentas.push(cuentaTransaccion);
+      }
+      let date = new Date();
+      date.setDate(1);
+      this.externalTransacccionDAOProvider.obtenerTransacciones(date).then(transacciones => {
+      }).catch(err => {
+        console.log("tenemos problemas para sincronizar la vaina", err);
+      });
+    })
+  }
+
+  private checkLocalDatabase() {
+    this.sqlite.create({
+      name: 'pesonalWallet.db',
+      location: 'default'
+    }).then((db: SQLiteObject) => {
+      this.db = db;
+      //this.borrarTablas();
+
+      db.executeSql('SELECT * FROM tbl_categorias', []).then(() => {
+        console.log('existen datos creados');
+        this.setearCategorias();
+      }).catch(e => {
+        console.log(e);
+        this.poblarBD();
+      });
+    }).catch(e => console.log(e));
+  }
+
+  public borrarTablas() {
+    this.db.executeSql('DROP TABLE IF EXISTS tbl_categorias', []).then(() => {
+      console.log('se borraron las categorias');
+      this.db.executeSql('DROP TABLE IF EXISTS tbl_cuentas', []).then(() => {
+        console.log('se borraron las cuentas');
+        this.db.executeSql('DROP TABLE IF EXISTS tbl_etiquetas', []).then(() => {
+          console.log('se borraron las etiquetas');
+          this.db.executeSql('DROP TABLE IF EXISTS tbl_transacciones', []).then(() => {
+            console.log('se borraron las transacciones');
+            this.db.executeSql('DROP TABLE IF EXISTS tbl_metas', []).then(() => {
+              console.log('se borraron las metas');
+              this.db.executeSql('DROP TABLE IF EXISTS tbl_presupuestos', []).then(() => {
+                console.log('se borraron las presupuestos');
+                this.poblarBD();
+              }).catch(e => {
+                console.log(e);
+              });
+            }).catch(e => {
+              console.log(e);
+            });
+          }).catch(e => {
+            console.log(e);
+          });
+        }).catch(e => {
+          console.log(e);
+        });
+      }).catch(e => {
+        console.log(e);
+      });
+    }).catch(e => {
+      console.log(e);
+    });
+  }
+
+  private poblarBD() {
+    console.log("poblando datos");
+    this.http.get('assets/sql/db.sql', { responseType: 'text' }).subscribe(sql => {
+      console.log("data", sql);
+      this.sqliteporter.importSqlToDb(this.db, sql).then(() => {
+        console.log("se creo bien la bd");
+        this.setearCategorias();
+      }).catch(e => {
+        alert("Error al importar la base de datos");
+        console.error("Error al importar la base de datos", e.message);
+      });
+    });
+  }
+
+  public setearCategorias() {
+    this.db.executeSql('SELECT * FROM tbl_categorias ORDER BY id_categoria', []).then(categorias => {
+      console.log('categorias', categorias);
+      for (var i = 0; i < categorias.rows.length; i++) {
+        let categoria = categorias.rows.item(i);
+        let categoriaTransaccion = CategoriaTransaccion.crearCategoria(categoria.id_categoria, categoria.nombre, categoria.icono, categoria.color, false, categoria.tipo, categoria.padre_categoria_id);
+        this.organizarSubcategorias(categoriaTransaccion);
+        //this.categoriaProvider.categorias.push(categoriaTransaccion);
+      }
+      this.setearCuentas();
+    }).catch(e => {
+      console.log(e);
+    });
+  }
+
+  public organizarSubcategorias(categoria:CategoriaTransaccion) {
+    if(categoria.padreId == null) {
+      console.log("es categoria", categoria);
+      this.categoriaProvider.categorias.push(categoria);
+    } else {
+      console.log("es subcategoria", categoria);
+      this.categoriaProvider.categorias.forEach(categoriaItem => {
+        if(categoriaItem.id == categoria.padreId) {
+          console.log("es subcategoria de la categoria", categoriaItem);
+          categoriaItem.subcategorias.push(categoria);
+        }
+      });
+    }
+  }
+
+  public setearCuentas() {
+    this.db.executeSql('SELECT * FROM tbl_cuentas', []).then(cuentas => {
+      console.log('cuentas', cuentas);
+      for (var i = 0; i < cuentas.rows.length; i++) {
+        let cuenta = cuentas.rows.item(i);
+        this.cuentaProvider.cuentas.push(CuentaTransaccion.crearCuenta(cuenta.id_cuenta, cuenta.nombre, cuenta.icono, cuenta.color, false, cuenta.tipo, cuenta.ingresos, cuenta.gastos, cuenta.transacciones, [], cuenta.saldo_total));
+      }
+      this.setearEtiquetas();
+    }).catch(e => {
+      console.log(e);
+    });
+  }
+
+  public setearEtiquetas() {
+    this.db.executeSql('SELECT * FROM tbl_etiquetas', []).then(etiquetas => {
+      console.log('etiquetas', etiquetas);
+      for (var i = 0; i < etiquetas.rows.length; i++) {
+        let etiqueta = etiquetas.rows.item(i);
+        this.etiquetaProvider.etiquetas.push(SelectFilterItem.crearSelectFilterItem(etiqueta.nombre, etiqueta.icono, etiqueta.color, false));
+      }
+      //this.db.close();
+      this.databaseReady.next(true);
+    }).catch(e => {
+      console.log(e);
+    });
+  }
+}
--- src/providers/database/externalDatabase/external-transacciones-dao-provider.ts
+++ src/providers/database/externalDatabase/external-transacciones-dao-provider.ts
@@ -1,11 +1,54 @@
 import { HttpClient } from '@angular/common/http';
 import { Transaccion } from './../../../objects/Transaccion';
 import { Injectable } from '@angular/core';
+import { EtiquetaProvider } from '../../etiqueta/etiqueta-provider';
+import { CuentaProvider } from '../../cuenta/cuenta-provider';
+import { CategoriasProvider } from '../../categorias/categorias-provider';
+import { TransaccionesProvider } from '../../transacciones/transacciones-provider';
 
 @Injectable()
 export class ExternalTransacccionDAOProvider {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+    public categoriaProvider: CategoriasProvider, 
+    public cuentaProvider:CuentaProvider, 
+    public etiquetaProvider:EtiquetaProvider,
+    public transaccionesProvider: TransaccionesProvider) { }
+
+  public obtenerTransacciones(fecha: Date) {
+    return new Promise((resolve, reject) => {
+      console.log("fecha a buscar", fecha.toISOString());
+      this.http.get('/remote/transacciones/listar/' + fecha.toISOString()).subscribe(transacciones => {
+        console.log("tenemos transacciones!!!", transacciones);
+        for(let index in transacciones) {
+          let transaccionDb = transacciones[index];
+          let date = new Date(transaccionDb.fecha);
+          let fecha = date.getFullYear() + "/" + (((date.getMonth() + 1) < 9) ? "0" + (date.getMonth() + 1): (date.getMonth() + 1)) + "/" + date.getDate();
+          let transaccion = Transaccion.crearTransaccion('gasto', transaccionDb.descripcion, transaccionDb.valor, fecha, transaccionDb.fijo, transaccionDb.realizado, transaccionDb.repeticion, null, null, null, transaccionDb.observacion, transaccionDb.ubicacion);
+          console.log("tran creada", transaccion);
+          for(let index in this.categoriaProvider.categorias) {
+            if(this.categoriaProvider.categorias[index].id == transaccionDb.categoria_id) {
+              console.log(this.categoriaProvider.categorias[index].id, this.categoriaProvider.categorias[index].itemGui);
+              transaccion.categoria = this.categoriaProvider.categorias[index].itemGui;
+              break;
+            }
+          }
+          for(let index in this.cuentaProvider.cuentas) {
+            console.log(this.cuentaProvider.cuentas[index].id, this.cuentaProvider.cuentas[index].itemGui);
+            if(this.cuentaProvider.cuentas[index].id == transaccionDb.cuenta_id) {
+              transaccion.cuenta = this.cuentaProvider.cuentas[index].itemGui;
+              break;
+            }
+          }
+          this.transaccionesProvider.transacciones.push(transaccion);
+        }
+        resolve("ok");
+      }, err => {
+        console.log("error en transacciones", err);
+        reject(err);
+      });
+    });
+  }
 
   public crearTransaccion(transaccion: Transaccion) {
     return new Promise((resolve, reject) => {
