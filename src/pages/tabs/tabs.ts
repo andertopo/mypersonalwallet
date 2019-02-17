41a4e7fe11f6b82b95ca756a0abc15d2c3dc66ef
andertopo <andersontorres9012@gmail.com>
2019-02-17 11:48:21 -0500

    se agrega pagina adicional al tabs

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { InicioPage } from '../inicio/inicio';
 import { ConfiguracionPage } from '../cofiguracion/configuracion';
 import { TransaccionesPage } from '../transacciones/transacciones';
+import { OtherPage } from '../otherPage/other-page';
+import { OtherTransactionsPage } from '../other-transactions/other-transactions';
 @Component({
   templateUrl: 'tabs.html'
 })
@@ -11,6 +13,7 @@ export class TabsPage {
   tab1Root = InicioPage;
   tab2Root = TransaccionesPage;
   tab3Root = ConfiguracionPage;
+  tab4Root = OtherPage;
 
   constructor() {
 
