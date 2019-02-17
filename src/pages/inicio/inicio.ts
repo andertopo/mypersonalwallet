b4281c921037c08215627fdf13f2f22e9f6641e7
andertopo <andersontorres9012@gmail.com>
2019-02-17 11:46:50 -0500

    se agrega submenú en un ion-card a manera de prueba de concepto

--- src/pages/inicio/inicio.ts
+++ src/pages/inicio/inicio.ts
@@ -1,7 +1,7 @@
 import { TabProvider } from './../../providers/tab/tab-provider';
 
 import {Component} from "@angular/core";
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
 import { PopoverController } from 'ionic-angular';
 
 import { CalendarioInicioPage } from './calendario-inicio-page';
@@ -12,6 +12,11 @@ import { TransaccionesPage } from "../transacciones/transacciones";
   templateUrl: 'inicio.html'
 })
 export class InicioPage {
+  public clase: string = '';
+  public arrowLateralSubmenu: string = 'arrow-back';
+  public classClose: string = '';
+
+
   public barChartOptions:any = {
     responsive: true,
     legend: {
@@ -68,7 +73,7 @@ export class InicioPage {
     console.log(e);
   }
   
-  constructor(public navCtrl: NavController, public popoverCtrl: PopoverController, public tabProvider: TabProvider) {
+  constructor(public navCtrl: NavController, public popoverCtrl: PopoverController, public tabProvider: TabProvider, public toast: ToastController) {
 
   }
   
@@ -82,5 +87,57 @@ export class InicioPage {
   goTransacciones(type:string) {
     this.tabProvider.typeTransaction.transaccion = type;
     this.navCtrl.parent.select(1);
+  }
+
+  quitarClase() {
+    console.log(document.getElementsByClassName('menu')[0].parentElement.classList.toggle("close"));
+  }
+
+  
+
+  cambiarClase() {
+    if(this.clase != '') {
+      this.clase = '';
+    } else {
+      this.clase = 'animacion';
+    }
+  }
+
+  toggleOption() {
+    document.getElementsByClassName('background-1')[0].classList.toggle('close');
+    document.getElementsByClassName('background-2')[0].classList.toggle('close');
+    document.getElementsByClassName('icon-animated')[0].classList.toggle('close');
+    document.getElementsByClassName('icon-container')[0].classList.toggle('close');
+
+    document.getElementsByClassName('icon-1')[0].classList.toggle('close');
+    document.getElementsByClassName('icon-2')[0].classList.toggle('close');
+    document.getElementsByClassName('icon-3')[0].classList.toggle('close');
+  }
+
+  
+  toggleLateralMenu() {
+    if(this.arrowLateralSubmenu == 'arrow-forward') {
+      this.arrowLateralSubmenu = 'arrow-back';
+      this.classClose = '';
+    } else {
+      this.arrowLateralSubmenu = 'arrow-forward';
+      this.classClose = 'close-content';
+    }
+    document.getElementsByClassName('links')[0].classList.toggle('close');
+    document.getElementsByClassName('lateral-submenu-icon-container')[0].classList.toggle('close');
+  }
+
+  clickOpcionT(message) {
+    console.log("click");
+    let toast = this.toast.create({
+      message: "click " + message,
+      duration: 3000,
+      position: 'middle'
+    });
+    toast.present();
+  }
+
+  scaleCard() {
+    document.getElementById('card').classList.toggle('close-card');
   }
 }
